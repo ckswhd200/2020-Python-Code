f4dc0bfed0b26a69558ac35bde34a1ede40ea4f6
ckswhd200 <63796128+ckswhd200@users.noreply.github.com>
2020-05-24 19:25:04 +0900

    Add files via upload

---
+++ ch04/04-01rideif.py
@@ -0,0 +1,3 @@
+height = 152
+if height >= 140:
+    print('롤러코스터 T-Express, 즐기세요!')
---
+++ ch04/04-02gradeif.py
@@ -0,0 +1,4 @@
+grade = float(input('1학기 평균 평점은? '))
+if grade >= 3.8:
+    print('축하합니다! 장학금 지급 대상자이다.')
+print('당신의 1학기 평균 평점 %.2f이다.'%(grade))
